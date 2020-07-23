902a5fb11354ee050ebb79087eb80ca7a6c19483
Gabriela <gaabrielaalmeida16@gmail.com>
2020-07-23 18:56:13 -0300

    Primeiro Teste

---
+++ unitarios/spec/avengers_spec.rb
@@ -0,0 +1,24 @@
+
+class AvengersHeadQuarter 
+    attr_accessor :list
+
+    def initialize
+        self.list = []
+    end
+    
+    def put(avenger)
+        self.list.push(avenger)
+    end
+
+end
+
+
+describe AvengersHeadQuarter do
+    
+    it "deve adiconar um vingador" do
+        hq = AvengersHeadQuarter.new
+
+        hq.put("Spiderman")
+        expect(hq.list).to eql ["Spiderman"]
+    end
+end
